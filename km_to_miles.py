0d238eaa1736b387466654d43011c9996cb1e630
Erik Braun <119681242+eBraunsWorld@users.noreply.github.com>
2023-01-20 20:29:07 -0500

    Add files via upload
    Convertor to change kilometers to miles. Used in part of the pace calculator.

--- km_to_miles.py
+++ km_to_miles.py
@@ -1,9 +1,13 @@
-km = float(input("How many kilometers?\n"))
-
-#Takes one variable to convert input kilometers into miles with only two decimal points.
-def km_to_miles(distance): 
-    miles = km / 1.609
-    miles = round(miles, 2)
-    return miles
-#Implement below code if you're only interested in the conversion and not running performance.
-#print(km, "kilometers is the same as", km_to_miles(km), "miles.")
+while True:
+    try:
+        km = float(input("Enter kilometers: "))
+        if km <= 0:
+            raise ValueError
+        def km_to_miles(distance):
+            miles = km / 1.609
+            miles = round(miles , 2)
+            return miles
+        print(km, "kilometers is the same as", km_to_miles(km), "miles.")
+        break
+    except ValueError:
+        print("ERROR: Type only positive numbers.")
